79ad606c4355368be8cbe0832c1065e92fc3b96c
drypot <drypot@gmail.com>
2021-09-18 10:36:06 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# RaySoda Front React
+
+RaySoda front React version.
+
+## Backend
+
+<https://github.com/drypot/raysoda>
+
+## History
+
+2021.09.18 React 버전 작업 시작.\
+2021.08.01 Backend 에서 Front 분리.
+
+## License
+
+[MIT](LICENSE)
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+
+function App() {
+  return (
+    <div className="App">
+      <div className="flex justify-center mt-8">
+        <p className="text-4xl">Hello Vite + React!</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
