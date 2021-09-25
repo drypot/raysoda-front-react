df19167ba17ffe66e49f1c7e684ddd478e744b5f
drypot <drypot@gmail.com>
2021-09-26 03:25:15 +0900

    update image-list

--- src/image/ImageList.tsx
+++ src/image/ImageList.tsx
@@ -1,8 +1,8 @@
-import React, { Fragment, useEffect, useState } from 'react'
-import { Link, useLocation } from 'react-router-dom'
+import React, { Fragment, MouseEventHandler, useEffect, useState } from 'react'
+import { Link, useHistory, useLocation } from 'react-router-dom'
 import { Helmet } from 'react-helmet-async'
 import { config } from '../lib/config'
-import { limitNumber, numberFrom } from '../lib/primitive'
+import { limitNumber2 } from '../lib/primitive'
 import { UrlMaker } from '../lib/url2'
 
 export interface ImageHead {
@@ -29,78 +29,88 @@ export default function ImageList() {
 }
 
 export function ImageListComponent() {
-  const [list, setList] = useState<ImageHead[] | null>(null)
-  const [prev, setPrev] = useState('')
-  const [next, setNext] = useState('')
+  const history = useHistory()
   const query = new URLSearchParams(useLocation().search)
-  const p = limitNumber(numberFrom(query.get('p'), 1), 1, NaN)
-  const ps = limitNumber(numberFrom(query.get('ps'), 16), 1, 128)
-  const d = query.get('d')
+  const [page, setPage] = useState(limitNumber2(query.get('p'), 1, 1, NaN))
+  const [pageSize, setPageSize] = useState(limitNumber2(query.get('ps'), 16, 1, 128))
+  const [date, setDate] = useState(query.get('d'))
+  const [list, setList] = useState<ImageHead[]>([])
+  const [prevUrl, setPrevUrl] = useState('')
+  const [nextUrl, setNextUrl] = useState('')
+
+  // setVar 할 때마다 렌더링하는 문제를 확인. 정상인가. 옵티마이징할 수 있는가.
 
   useEffect(() => {
-    fetch(`/api/image?p=${p}&ps=${ps}&d=${d}`)
+    fetch(`/api/image?p=${page}&ps=${pageSize}&d=${date ?? ''}`)
       .then(res => res.json())
       .then(body => {
-        const list: ImageHead[] | undefined = body.list
-        if (list && list.length) {
-          setList(list)
-          setPrev(p > 1 ? UrlMaker.from('/image-list')
-            .add('d', d).add('p', p - 1, 1).add('ps', ps, 16).gen() : '')
-          setNext(list.length === ps ? UrlMaker.from('/image-list')
-            .add('d', d).add('p', p + 1).add('ps', ps, 16).gen() : '')
-        }
-        console.log(prev)
-        console.log(next)
+        const list: ImageHead[] = body.list ?? []
+        setList(list)
+        setPrevUrl(page > 1 ? UrlMaker.from('/image-list')
+          .add('d', date).add('p', page - 1, 1).add('ps', pageSize, 16).gen() : '')
+        setNextUrl(list.length === pageSize ? UrlMaker.from('/image-list')
+          .add('d', date).add('p', page + 1).add('ps', pageSize, 16).gen() : '')
       })
-  }, [])
+  }, [page, pageSize, date])
 
-  if (list && list.length) {
-    return (
-      <Fragment>
-        <Helmet>
-          <meta property="og:image" content={list[0].thumbUrl || ''}/>
-        </Helmet>
-        <div className="mt-image-thumb max-w-full text-center">
-          {list.map(image =>
-            <div className="mt-image-thumb first:mt-0 link-no-deco" key={image.id}>
-              {/* 이미지 좌상단에 FullScreen 아이콘을 넣기 위해 inline-block 을 한다. */}
-              {/* inline-block 을 하면 div 크기가 내부 이미지 크기와 같아진다. */}
-              <div className="inline-block relative">
-                <Link to={'/image/' + image.id}>
-                  <img src={image.thumbUrl} className="max-w-full max-h-screen"/>
-                </Link>
-              </div>
-              {image.comment && <div className="mt-4">{image.comment.split(/\r\n|\n/, 1)[0]}</div>}
-              {/* user.home 에 '/' 있는 경우도 있으므로 인코딩한다. */}
-              <div className="mt-4"><Link to={'/user/' + encodeURIComponent(image.owner.home)}>{image.owner.name}</Link>
-              </div>
-            </div>
-          )}
-          <div className="mt-image-thumb flex justify-center">
-            {prev &&
-            <Link to={prev} className="mr-12">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24"
-                   stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7"/>
-              </svg>
-            </Link>
-            }
-            {next &&
-            <Link to={next}>
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24"
-                   stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7"/>
-              </svg>
-            </Link>}
-          </div>
-          <div className="mt-image-thumb flex justify-center space-x-6">
-            <a href="https://raysoda.com">RaySoda</a>
-            <a href="https://rapixel.com">Rapixel</a>
-            <a href="https://osoky.com">Osoky</a>
-          </div>
-        </div>
-      </Fragment>
-    )
+  const onClickPrev: MouseEventHandler<HTMLAnchorElement> = (e) => {
+    e.preventDefault()
+    history.push(prevUrl)
+    setPage(page - 1)
   }
-  return <></>
+
+  const onClickNext: MouseEventHandler<HTMLAnchorElement> = (e) => {
+    e.preventDefault()
+    history.push(nextUrl)
+    setPage(page + 1)
+  }
+
+  return (
+    <Fragment>
+      <Helmet>
+        {list.length && <meta property="og:image" content={list[0].thumbUrl || ''}/>}
+      </Helmet>
+      <div className="mt-image-thumb max-w-full text-center">
+        {list.length == 0 && <p>마지막 페이지입니다.</p>}
+        {list.map(image =>
+          <div className="mt-image-thumb first:mt-0 link-no-deco" key={image.id}>
+            {/* 이미지 좌상단에 FullScreen 아이콘을 넣기 위해 inline-block 을 한다. */}
+            {/* inline-block 을 하면 div 크기가 내부 이미지 크기와 같아진다. */}
+            <div className="inline-block relative">
+              <Link to={'/image/' + image.id}>
+                <img src={image.thumbUrl} className="max-w-full max-h-screen"/>
+              </Link>
+            </div>
+            {image.comment && <div className="mt-4">{image.comment.split(/\r\n|\n/, 1)[0]}</div>}
+            {/* user.home 에 '/' 있는 경우도 있으므로 인코딩한다. */}
+            <div className="mt-4"><Link to={'/user/' + encodeURIComponent(image.owner.home)}>{image.owner.name}</Link>
+            </div>
+          </div>
+        )}
+        <div className="mt-image-thumb flex justify-center">
+          {prevUrl.length > 0 &&
+          <a href={prevUrl} onClick={onClickPrev} className="mr-12">
+            <svg xmlns="http://www.w3.org/2000/svg"
+                 className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7"/>
+            </svg>
+          </a>
+          }
+          {nextUrl.length > 0 &&
+          <a href={nextUrl} onClick={onClickNext}>
+            <svg xmlns="http://www.w3.org/2000/svg"
+                 className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7"/>
+            </svg>
+          </a>
+          }
+        </div>
+        <div className="mt-image-thumb flex justify-center space-x-6">
+          <a href="https://raysoda.com">RaySoda</a>
+          <a href="https://rapixel.com">Rapixel</a>
+          <a href="https://osoky.com">Osoky</a>
+        </div>
+      </div>
+    </Fragment>
+  )
 }
